76b1545f5986756560f37d94a963aee55d30f171
claes <rogermarcus04@gmail.com>
2025-04-20 00:14:34 +0200

    Add files via upload

---
+++ låna-2000-kronor/60-dagar/index.php
@@ -0,0 +1,419 @@
+
+<!doctype html>
+ 	
+
+<head>
+  <html  lang="sv-SE"> 
+		<style type="text/css" id="wp-custom-css">
+ .table-container tr th:first-child {
+    max-width: 200px;
+    color: black;
+}
+		.\31 hd {
+    margin-top: 20px;
+}
+ 
+	</style>
+	<PageMap>
+    <DataObject type="thumbnail">
+      <Attribute name="src" value="https://lastoffer.net/kronordagar/2000kronor-60dagar.jpg"/>
+      <Attribute name="width" value="300"/>
+      <Attribute name="height" value="300"/>
+    </DataObject>
+  </PageMap>		
+
+<link rel='dns-prefetch' href='//s.w.org' />
+ 
+ 
+<link rel='stylesheet' id='responsive-style-css' href='https://lastoffer.net/wp-content/themes/responsive/core/css/style.css' type='text/css' media='all' />
+ 
+ 
+
+ <meta name="theme-color" content="#0f791a"> <meta name="msapplication-navbutton-color" content="#0f791a"> <meta name="apple-mobile-web-app-status-bar-style" content="#0f791a"><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Snabblån 2 000 kronor i två månader (60 dagar). Jämför pris</title>
+
+<meta name="description" content="Prisjämförelse snabblån & sms lån på 2 000 kronor i två månader (60 dagar) två månader. Billigast att låna 2 000 kronor just nu ... "/>
+ 
+ <link rel="canonical" href="https://lastoffer.net/låna-2000-kronor/60-dagar/" /> 
+ 
+</head>
+<body class="home page-template-default page page-id-406 custom-background">
+
+
+<div id="container" class="hfeed">
+ 
+
+<header class="header">
+		   <a href="https://lastoffer.net/" class="logo"><img alt="Lastoffer" title="Lastoffer" src="https://lastoffer.net/media/lnw97.webp"></a>   
+  </div>
+ 
+	
+ 
+
+</header> 
+<div id="content-outer" > <div id="wrapper" class="clearfix"><div id="content-outer">
+ 
+<div id="wrapper" >
+<div id="content-full" >
+	<div class="1hd">  
+<h1>Låna 2 000 kronor i två månader (60 dagar)</h1>
+			<div class="hblp"><div id="date">2025-04-19</div></div></div>	
+	<div class="post-entry">
+<div >
+	<div class="row"> <div class="cell">
+<div class="cinfo1">
+
+ 
+	<p><b>Snabblån 2000 kronor</b> med två månaders löptid. Jämför korttidslån med eller utan UC, betalningsanmärkningar och kreditupplysning.<p>
+	
+	<p>Här finns snabba mikrolån med löptid: <b>två månader / 60 dagar</b>. Tänk på att för korta lån är avgiften viktigare än räntesatsen.</p>
+	</div></div></div>
+	<div class="row">
+<div class="cell right">
+
+			 <img  src="https://lastoffer.net/kronordagar/2000kronor-60dagar.jpg" width="300" height="300" alt="Låna 2 000 kronor i två månader" title="Låna pengar 2 000 kronor i två månader">   </div> </div>
+		 
+	  
+
+ 
+ <div class="rw"></div>
+	
+ <div class="skw"><h2><div class="unskew">Snabblån <span class="mobile-break"><br></span>2 000 kronor  i två månader</div></h2></div>
+	
+ 
+	 <div class="cell block rel">
+		 <div class="table-container"> 
+ <table class="data-table">
+		<tr>
+ 	<td> </td><td>Totalt</td><td>Exempel</td><td>Uppläggning</td><td>Avi</td><td>Ränta</td>	<td>Check</td><td></td>
+	
+	</tr>
+	
+  
+			<tr>  <td><a href="https://www.ferratum.se" target="_blank" rel="sponsored"><img src=" https://lastoffer.net//adbilder/ferratum.webp" width="150" height="50" title="Ferratum Pris 131 Kr [ränta 131Kr - Uppläggningsavgift: 0 kr Aviavgift: 0 kr Uttagsavgift: 0 kr]"></a></td><td>131 Kr</td><td>53,3 %</td><td>0 Kr</td><td>0 Kr </td><td>131 Kr</td><td>Creditsafe<td><a href="/ferratum/">Ferratum</a></td>	</tr>
+		  
+			<tr>  <td><a href="https://northmill.com/se/kontokredit/affiliate" target="_blank" rel="sponsored"><img src=" https://lastoffer.net//adbilder/northmill.webp" width="150" height="50" title="Northmill Pris 275 Kr [ränta 42Kr - Uppläggningsavgift: 195 kr Aviavgift: 19 kr Uttagsavgift: 0 kr]"></a></td><td>275 Kr</td><td>16.9 %</td><td>195 Kr</td><td>19 Kr </td><td>42 Kr</td><td>Flera<td><a href="/northmill/">Northmill</a></td>	</tr>
+		  
+			<tr>  <td><a href="https://daypay.se" target="_blank" rel="sponsored"><img src=" https://lastoffer.net//adbilder/daypay.webp" width="150" height="50" title="Daypay Pris 450 Kr [ränta 96Kr - Uppläggningsavgift: 276 kr Aviavgift: 39 kr Uttagsavgift: 0 kr]"></a></td><td>450 Kr</td><td>39 %</td><td>276 Kr</td><td>39 Kr </td><td>96 Kr</td><td>Creditsafe<td><a href="/daypay/">Daypay</a></td>	</tr>
+		  
+			<tr>  <td><a href="https://www.smspengar.nu" target="_blank" rel="sponsored"><img src=" https://lastoffer.net//adbilder/smspengar.webp" width="150" height="50" title="SMSPengar Pris 478 Kr [ränta 108Kr - Uppläggningsavgift: 280 kr Aviavgift: 45 kr Uttagsavgift: 0 kr]"></a></td><td>478 Kr</td><td>44 %</td><td>280 Kr</td><td>45 Kr </td><td>108 Kr</td><td>Creditsafe<td><a href="/sms-pengar/">SMSPengar</a></td>	</tr>
+		  
+			<tr>  <td><a href="https://www.viaconto.se" target="_blank" rel="sponsored"><img src=" https://lastoffer.net//adbilder/viaconto.webp" width="150" height="50" title="Viaconto Pris 567 Kr [ränta 97Kr - Uppläggningsavgift: 380 kr Aviavgift: 45 kr Uttagsavgift: 0 kr]"></a></td><td>567 Kr</td><td>39.5 %</td><td>380 Kr</td><td>45 Kr </td><td>97 Kr</td><td>Creditsafe<td><a href="/viaconto/">Viaconto</a></td>	</tr>
+		  
+			<tr>  <td><a href="https://gfmoney.se" target="_blank" rel="sponsored"><img src=" https://lastoffer.net//adbilder/gfmoney.webp" width="150" height="50" title="GFMoney Pris 573 Kr [ränta 108Kr - Uppläggningsavgift: 280 kr Aviavgift: 45 kr Uttagsavgift: 95 kr]"></a></td><td>573 Kr</td><td>44 %</td><td>280 Kr</td><td>45 Kr </td><td>108 Kr</td><td>Creditsafe<td><a href="/gfmoney/">GFMoney</a></td>	</tr>
+		  
+			<tr>  <td><a href="https://binly.se" target="_blank" rel="sponsored"><img src=" https://lastoffer.net//media/binly.webp" width="150" height="50" title="Binly Pris 710 Kr [ränta 105Kr - Uppläggningsavgift: 485 kr Aviavgift: 60 kr Uttagsavgift: 0 kr]"></a></td><td>710 Kr</td><td>42.5 %</td><td>485 Kr</td><td>60 Kr </td><td>105 Kr</td><td>Creditsafe<td><a href="/binly/">Binly</a></td>	</tr>
+		  
+			<tr>  <td><a href="https://www.lumify.se" target="_blank" rel="sponsored"><img src=" https://lastoffer.net//adbilder/lumify.webp" width="150" height="50" title="Lumify Pris 915 Kr [ränta 110Kr - Uppläggningsavgift: 685 kr Aviavgift: 60 kr Uttagsavgift: 0 kr]"></a></td><td>915 Kr</td><td>44.5 %</td><td>685 Kr</td><td>60 Kr </td><td>110 Kr</td><td>Creditsafe<td><a href="/lumify/">Lumify</a></td>	</tr>
+		 	</table>
+	
+ </div></div></div>
+	
+	
+  <div class="lan"><h2><div class="unskew">Billigast  2 000 kronorslån  i två månader har Ferratum</div></h2></div>
+	
+	<div class="cell"><p>I vår prisjämförelse av 8 lån på 2 000 kronor i  två månader är det just nu billigast att låna från Ferratum.</p>
+	
+		<p>Det kostar  131 kronor för ett <a href="https://lastoffer.net/snabblan/">snabblån</a> på 2 000 kronor från Ferratum med återbetalning inom två månader. </p>
+	</div><div class="cell"><h3>Kostnader Ferratum</h3>
+ 
+ 
+	
+	
+<p>Uppläggningsavgift:	0 Kr</p>
+<p>Aviavgift:	0 Kr</p>
+<p>Uttagsavgift:	0 Kr</p>
+<p>Räntekostnad:	131 Kr</p>	
+<p>Totalt:	131 Kr</p>		
+</div><div class="cell">	<h3>Vanlig kreditsituation</h3>
+ 
+
+<p>Exempelränta:	53,3 %</p>
+ 
+	 <p>Belopp:	25 000 Kr</p>
+	
+	<p><a href="https://www.ferratum.se"> Ferratum</a> tar kreditupplysning via Creditsafe	</table>
+	</div>
+<div class="row"> 
+ <div class="skw"><h2><div class="unskew">Ändra belopp</div></h2></div>
+	
+			
+	<a href="https://lastoffer.net/låna-1000-kronor/60-dagar/"><img src="https://lastoffer.net/kronordagar/lana-1000-kronor.png" alt="Låna 1000 kronor i två månader  dagar"/></a>
+		<br>
+		
+			 
+		<img src="https://lastoffer.net/kronordagar/lana-2000-kronor.png" style="border-color:red; border-style:solid; border-width:3px;" alt="Låna 2000 kronor i två månader  dagar"/>
+		<br>
+		
+					
+	<a href="https://lastoffer.net/låna-3000-kronor/60-dagar/"><img src="https://lastoffer.net/kronordagar/lana-3000-kronor.png" alt="Låna 3000 kronor i två månader  dagar"/></a>
+		<br>
+		
+					
+	<a href="https://lastoffer.net/låna-4000-kronor/60-dagar/"><img src="https://lastoffer.net/kronordagar/lana-4000-kronor.png" alt="Låna 4000 kronor i två månader  dagar"/></a>
+		<br>
+		
+					
+	<a href="https://lastoffer.net/låna-5000-kronor/60-dagar/"><img src="https://lastoffer.net/kronordagar/lana-5000-kronor.png" alt="Låna 5000 kronor i två månader  dagar"/></a>
+		<br>
+		
+					
+	<a href="https://lastoffer.net/låna-6000-kronor/60-dagar/"><img src="https://lastoffer.net/kronordagar/lana-6000-kronor.png" alt="Låna 6000 kronor i två månader  dagar"/></a>
+		<br>
+		</div><div class="row"> 
+ <div class="skw"><h2><div class="unskew">Ändra period</div></h2></div>
+						<a href="https://lastoffer.net/låna-2000-kronor/30-dagar/"><img src="https://lastoffer.net/media/30-dagar.png" alt="Låna 2 000kronor i 30 dagar"/></a>
+		 		 
+		
+					 
+		<img src="https://lastoffer.net/media/60-dagar.png" style="border-color:red; border-style:solid; border-width:3px;" alt="Låna 2 000 kronor i 60 dagar"/>
+		 
+		
+								<a href="https://lastoffer.net/låna-2000-kronor/90-dagar/"><img src="https://lastoffer.net/media/90-dagar.png" alt="Låna 2 000kronor i 90 dagar"/></a>
+		 		 
+		
+								<a href="https://lastoffer.net/låna-2000-kronor/180-dagar/"><img src="https://lastoffer.net/media/180-dagar.png" alt="Låna 2 000kronor i 180 dagar"/></a>
+		 		 
+		
+		</div>
+		<div class="rw"></div>
+		
+		
+		<div class="row"><div class="lan">
+<h2><div class="unskew">Låna 2 000 i två månader dagar utan UC</div></h2></div>
+	
+	  
+
+<div class="cell"><p>Här kan få ett smslån / <b>snabblån 2 000 kronor utan UC i två månader</b>. Genom att välja en långivare som erbjuder <a href="https://lastoffer.net/lan-utan-uc/" class="in">snabbån utan UC</a> går det att slippa en registrering hos Upplysningscentralen.</p></div></div>	<div class="row">
+		
+					 <div class="cell right"> <img  src="https://lastoffer.net/media/lana-pengar-utan-uc.gif" width="300" height="300" alt="Snabblån 2 000 i två månader dagar utan UC>">  </div>    </div>   
+ 
+	
+  <div class="row">
+	<div class="lan"><h2><div class="unskew">Lån 2 000 kronor  i två månader dagar med betalningsanmärkning</div></h2></div> 
+
+	<div class="cell"> 
+	
+ <p>Genom att välja en långivare som erbjuder <a href="https://lastoffer.net/lana-pengar-betalningsanmarkning/" class="in">snabblån med betalningsanmärkning</a> går det att få ett lån trots en eller flera anmärkningar hos kronofogden.</p>	 </div> </div>
+		  <div class="row"> <div class="cell  right"> <img  src="https://lastoffer.net/media/lana-pengar-med-betalningsanmarkning.webp" width="300" height="300" alt="Snabblån 2 000 kronor  i två månader dagar med betalningsanmärkning"> </div>  </div>    
+ 
+	
+ 
+ <div class="rw">
+	<h2 class="questions"><div class="unskew">Frågor och svar</div></h2></div>
+	<div class="cell hundra"><dl class="faq">
+<dt><button type="button" aria-expanded="false" aria-controls="faq 1_desc">Var kan jag låna  2 000 kronor i två månader?</button></dt>
+<dd><div id="faq 1_desc" class="desc">På lastoffer.net finns långivare som erbjuder lån på  2 000 kronor i två månader sorterade efter pris.</button></dt>
+<dt><button type="button" aria-expanded="false" aria-controls="faq 2_desc">Kan jag låna 2 000 kronor i 60 dagar?</button></dt>
+<dd><div id="faq 2_desc" class="desc">Det går utmärkt att låna  2 000 kronor i 60 dagar. Kika in på långuide.se så hittar du alla lån sorterade efter pris.</button></dt>
+	
+<dt><button type="button" aria-expanded="false" aria-controls="faq 3_desc">Går det att jämföra priser för ett snabblån på 2 000 i två månader?</button></dt>
+<dd><div id="faq 3_desc" class="desc">Prisjämförelse för ett snabblån på 2 000 kronor uppdaterad i två månader finns på långuide.se</button></dt>
+	
+<dt><button type="button" aria-expanded="false" aria-controls="faq 4_desc">Vad kostar ett sms lån på 2 000 i två månader?</button></dt>
+<dd><div id="faq 1_desc" class="desc">Kostnad för ett sms lån på 2 000 kronor uppdaterad i två månader finns på lastoffer.net</div></dd></dl></div>
+	
+	
+	
+	
+	<script type="application/ld+json">{"@context":"https://schema.org","@type":"FAQPage","mainEntity":[{"@type":"Question","name":"Var kan jag låna  2 000 kronor i två månader?","acceptedAnswer":{"@type":"Answer","text":"På långuide.se finns långivare som erbjuder lån på  2 000 kronor i två månader sorterade efter pris."}},{"@type":"Question","name":"Kan jag låna 2 000 kronor i 60 dagar?","acceptedAnswer":{"@type":"Answer","text":"Det går utmärkt att låna 
+	
+	
+	2 000 kronor i 60 dagar. Kika in på långuide.se så hittar du alla lån sorterade efter pris"}},{"@type":"Question","name":"Går det att jämföra priser för ett snabblån på 2 000 i två månader?","acceptedAnswer":{"@type":"Answer","text":"Prisjämförelse för ett snabblån på 2 000 kronor uppdaterad i två månader finns på långuide.se"}},{"@type":"Question","name":"Vad kostar ett sms lån på 2 000 i två månader?","acceptedAnswer":{"@type":"Answer","text":"Kostnad för ett sms lån på 2 000 kronor uppdaterad i två månader finns på långuide.se"}}]}</script> 
+</div>
+ 
+</div>
+</div>
+</div>
+</div>
+ 
+  <div class="foot3"> 
+
+<div class="ftbox"><h4>Belopp snabblån</h4>
+	
+<p><a href="https://lastoffer.net/låna-1000-kronor/30-dagar/">låna 1000 kronor</a> </p>
+<p><a href="https://lastoffer.net/låna-2000-kronor/30-dagar/">låna 2000 kronor</a> </p>
+	<p><a href="https://lastoffer.net/låna-3000-kronor/30-dagar/">låna 3000 kronor</a> </p>
+	<p><a href="https://lastoffer.net/låna-4000-kronor/30-dagar/">låna 4000 kronor</a> </p>
+	<p><a href="https://lastoffer.net/låna-5000-kronor/30-dagar/">låna 5000 kronor</a> </p>
+	<p><a href="https://lastoffer.net/låna-6000-kronor/30-dagar/">låna 6000 kronor</a> </p>
+</div>
+ 
+<div class="ftbox"><h4>Mer om oss</h4>
+<p><a href="https://lastoffer.net/kontakta-oss/">Kontakt</a></p> 
+ 
+</div>
+	
+		<div class="ftbox"><h4>Utgivare</h4>
+	<p>Lastoffer Network</p>
+				<p>info@lastoffer.net</p>
+			 <p>© 2003-25</p>
+		 
+ </div> 
+	<div class="bajo">
+	
+	<div class="ft3"> 
+ 	<p>  <a href="https://lastoffer.net"><img src="https://lastoffer.net/media/lnw97.webp" width="80" height="80" alt="lastoffer.net"></a> </p>
+</div>
+ 
+	</div>
+	
+
+</div>
+
+ 
+	<div id="backtop" data-v-w="200,200">&#9650;</div>
+</div></div>	
+ 
+<script type="text/javascript" src="https://cdn.adt558.com/atag.js?as=1047349355" charset="UTF-8"></script>
+ 
+ 		 
+ 
+ <script> 
+'use strict';
+ 
+class DisclosureButton {
+  constructor(buttonNode) {
+    this.buttonNode = buttonNode;
+    this.controlledNode = false;
+
+    var id = this.buttonNode.getAttribute('aria-controls');
+
+    if (id) {
+      this.controlledNode = document.getElementById(id);
+    }
+
+    this.buttonNode.setAttribute('aria-expanded', 'false');
+    this.hideContent();
+
+    this.buttonNode.addEventListener('click', this.onClick.bind(this));
+    this.buttonNode.addEventListener('focus', this.onFocus.bind(this));
+    this.buttonNode.addEventListener('blur', this.onBlur.bind(this));
+  }
+
+  showContent() {
+    if (this.controlledNode) {
+      this.controlledNode.style.display = 'block';
+    }
+  }
+
+  hideContent() {
+    if (this.controlledNode) {
+      this.controlledNode.style.display = 'none';
+    }
+  }
+
+  toggleExpand() {
+    if (this.buttonNode.getAttribute('aria-expanded') === 'true') {
+      this.buttonNode.setAttribute('aria-expanded', 'false');
+      this.hideContent();
+    } else {
+      this.buttonNode.setAttribute('aria-expanded', 'true');
+      this.showContent();
+    }
+  }
+ 
+
+  onClick() {
+    this.toggleExpand();
+  }
+
+  onFocus() {
+    this.buttonNode.classList.add('focus');
+  }
+
+  onBlur() {
+    this.buttonNode.classList.remove('focus');
+  }
+}
+ 
+
+window.addEventListener(
+  'load',
+  function () {
+    var buttons = document.querySelectorAll(
+      'button[aria-expanded][aria-controls]'
+    );
+
+    for (var i = 0; i < buttons.length; i++) {
+      new DisclosureButton(buttons[i]);
+    }
+  },
+  false
+);
+</script>
+ <script> 
+class DisclosureButton {
+  constructor(buttonNode) {
+    this.buttonNode = buttonNode;
+    this.controlledNode = false;
+
+    var id = this.buttonNode.getAttribute('aria-controls');
+
+    if (id) {
+      this.controlledNode = document.getElementById(id);
+    }
+
+    this.buttonNode.setAttribute('aria-expanded', 'false');
+    this.hideContent();
+
+    this.buttonNode.addEventListener('click', this.onClick.bind(this));
+    this.buttonNode.addEventListener('focus', this.onFocus.bind(this));
+    this.buttonNode.addEventListener('blur', this.onBlur.bind(this));
+  }
+
+  showContent() {
+    if (this.controlledNode) {
+      this.controlledNode.style.display = 'block';
+    }
+  }
+
+  hideContent() {
+    if (this.controlledNode) {
+      this.controlledNode.style.display = 'none';
+    }
+  }
+
+  toggleExpand() {
+    if (this.buttonNode.getAttribute('aria-expanded') === 'true') {
+      this.buttonNode.setAttribute('aria-expanded', 'false');
+      this.hideContent();
+    } else {
+      this.buttonNode.setAttribute('aria-expanded', 'true');
+      this.showContent();
+    }
+  }
+
+  /* EVENT HANDLERS */
+
+  onClick() {
+    this.toggleExpand();
+  }
+
+  onFocus() {
+    this.buttonNode.classList.add('focus');
+  }
+
+  onBlur() {
+    this.buttonNode.classList.remove('focus');
+  }
+}
+
+/* Initialize Hide/Show Buttons */
+
+window.addEventListener(
+  'load',
+  function () {
+    var buttons = document.querySelectorAll(
+      'button[aria-expanded][aria-controls]'
+    );
+
+    for (var i = 0; i < buttons.length; i++) {
+      new DisclosureButton(buttons[i]);
+    }
+  },
+  false
+);
+</script>
+		<script src="https://lastoffer.net/css/float-panel.js"></script>
+ 
+</body></html> 
